b5f4be6a0bca49729e20ee5329df5f0b62aaa5f0
1002472 <1002472@SKP1002472NM003.local>
2017-09-21 16:14:59 +0900

    step1

--- src/reducer/initialState.js
+++ src/reducer/initialState.js
@@ -1,17 +1,5 @@
 export default {
-    authorReducer: {
-        authors: []
-    },
-
     courseReducer: {
         courses: []
-    },
-
-    selectedCourseReducer: {
-        course: undefined
-    },
-
-    apiReducer: {
-        apiCallsInProgress: 0
     }
 }
